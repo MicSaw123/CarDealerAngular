22fe137174526c97a317f10a96a8e8aefebac311
MicSaw123 <mic_saw123@o2.pl>
2024-06-24 21:43:47 +0200

    CarDealerAngular cleaned

---
+++ src/app/CarDealerAngular.ApiHandlers/GenericApiService/BaseApiRequests.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment.development";
+import {catchError, Observable, throwError} from "rxjs";
+
+@Injectable()
+
+export class BaseApiRequestsService{
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  get<T>(url: string){
+    return this.http.get<T>(`${this.apiUrl}/${url}`).pipe(catchError(this.formatErrors));
+  }
+  post<T>(url: string, body: any): Observable<T>{
+    return this.http.post<T>(`${this.apiUrl}/${url}`, body).pipe(catchError(this.formatErrors));
+  }
+
+  put<T>(url: string, body: any){
+    return this.http.put<T>(`${this.apiUrl}/${url}`, body).pipe(catchError(this.formatErrors));
+  }
+
+  delete<T>(url: string){
+    return this.http.delete<T>(`${this.apiUrl}/${url}`).pipe(catchError(this.formatErrors));
+  }
+
+  private formatErrors(error: any): Observable<any> {
+    console.log(error);
+    return throwError(error.error);
+  }
+}
